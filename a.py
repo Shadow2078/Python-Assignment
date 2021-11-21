b4225c90bf980e5b9cb818cf0473a7cc3a442279
Shadow2078 <karkisuman2021@gmail.com>
2021-11-21 13:57:52 +0545

    Vsstudio commit

---
+++ a.py
@@ -0,0 +1,16 @@
+a=21
+b=10
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+if(a!=b):
+     print("a is not equal to b")
+else:
+     print("a is equal to b")
+
+if(a<b):
+    print("a is less than b")
+else:
+    print("a is not less than b")
